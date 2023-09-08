c8b8920a93ab5f5226473e16c394346ecf66a8d0
LeoHuari <leohuari2406@hotmail.com>
2023-09-08 12:00:39 -0300

    Agregando rama iniscripcion

--- src/universidadproject/Entidades/Inscripcion.java
+++ src/universidadproject/Entidades/Inscripcion.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadproject.Entidades;
 
-/**
- *
- * @author Leo Huari
- */
+
 public class Inscripcion {
-    
+    private int idInscripcion;
+    private double nota;
+    private Alumno alumno;
 }
