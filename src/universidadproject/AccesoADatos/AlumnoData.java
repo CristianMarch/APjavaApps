234293049db196b973343ddca5ef48f503205990
CristianMarch <kicho2002@hotmail.com>
2023-09-12 11:28:34 -0300

    .

--- src/universidadproject/AccesoADatos/AlumnoData.java
+++ src/universidadproject/AccesoADatos/AlumnoData.java
@@ -1,13 +1,8 @@
 package universidadproject.AccesoADatos;
 
-import java.sql.Connection;
 import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import universidadproject.Entidades.Alumno;
 
